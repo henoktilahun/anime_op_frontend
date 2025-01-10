dae02a79d0d57d3d4576c182ac1d7731523249cd
Henok Tilahun <henok.tilahun1996@gmail.com>
2025-01-10 08:54:29 -0500

    style cards

--- src/components/AnimeCard/AnimeCard.jsx
+++ src/components/AnimeCard/AnimeCard.jsx
@@ -2,18 +2,20 @@ import "./AnimeCard.css";
 
 function AnimeCard({ anime, song, image }) {
   return (
-    <div className="anime-card">
-      <img
-        className="anime-card__image"
-        src={image}
-        alt={`${anime.name} Cover`}
-      />
-      <div className="anime-card__details">
-        <h3 className="anime-card__title">{anime.name}</h3>
-        <p className="anime-card__song">{song.title}</p>
-        <p className="anime-card__synopsis">{anime.synopsis}</p>
+    <li className="card">
+      <div className="anime-card">
+        <img
+          className="anime-card__image"
+          src={image}
+          alt={`${anime.name} Cover`}
+        />
+        <div className="anime-card__details">
+          <h3 className="anime-card__title">{anime.name}</h3>
+          <p className="anime-card__song">{song.title}</p>
+          {/* <p className="anime-card__synopsis">{anime.synopsis}</p> */}
+        </div>
       </div>
-    </div>
+    </li>
   );
 }
 
