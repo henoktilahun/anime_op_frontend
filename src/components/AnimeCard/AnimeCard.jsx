6c568eb7990f8967b349c14f997d509082d9ac11
Henok Tilahun <henok.tilahun1996@gmail.com>
2025-01-30 09:40:25 -0500

    style card and add play button

--- src/components/AnimeCard/AnimeCard.jsx
+++ src/components/AnimeCard/AnimeCard.jsx
@@ -1,6 +1,7 @@
 import "./AnimeCard.css";
 import { Link } from "react-router-dom";
 import Rating from "../Rating/Rating";
+import playButton from "../../assets/play-button.png";
 
 function AnimeCard({ id, anime, song, image, type }) {
   //console.log(song?.artists?.[0]?.name || "Unkown Artist");
@@ -20,15 +21,30 @@ function AnimeCard({ id, anime, song, image, type }) {
           alt={`${anime.name} Cover`}
         />
       </Link>
+      <div className="playButton">
+        <div className="circle">
+          <div className="triangle"></div>
+        </div>
+      </div>
+
+      {/* <div className="play-button__container">
+        <button className="play-button">
+          <img
+            className="play-button__image"
+            src={playButton}
+            alt="Play Button"
+          />
+        </button>
+      </div> */}
       <div className="anime-card__details">
         <Link to={`/${id}`}>
-          <h3 className="anime-card__song">{song.title}</h3>
-          <p className="anime-card__title">
+          <h2 className="anime-card__song">{song.title}</h2>
+          <p className="anime-card__artist">
             by: {song?.artists?.[0]?.name || "Unkown Artist"}
           </p>
-          <p className="anime-card__title">
+          {/* <p className="anime-card__title">
             Anime: {anime.name} Type: {type}
-          </p>
+          </p> */}
         </Link>
         <Rating defaultRating={defaultRating} />
 
